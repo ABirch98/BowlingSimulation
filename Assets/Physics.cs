b88e9b341278b3d74c2d290c7f06674de0d082fd
Adam Birch <adam.birch@workmail.com>
2019-12-30 23:06:15 +0000

    v3
    Physics works somewhat need to add time integral

--- Assets/Physics.cs
+++ Assets/Physics.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-//https://www.toptal.com/game/video-game-physics-part-i-an-introduction-to-rigid-body-dynamics
 public class Physics : MonoBehaviour
 {
     public GameObject Ball;
@@ -29,7 +27,7 @@ public class Physics : MonoBehaviour
     private Vector3 BallForce;
     private Vector3 A;
     private Vector3 B;
-    private float InitialXForce = -2.0f;
+    private float InitialXForce = -8.0f;
     private float momentum;
     private float CentripetalAcceleration;
     private float KineticFriction;
@@ -37,10 +35,10 @@ public class Physics : MonoBehaviour
     private float Gravity = -9.81f;
     private Vector3 VectorVelocity;
     private float PinDistance;
-    private float collisionTime = 0;
     private float currentTime = 0;
     private float OldTime = 0;
     private float dt = 1;
+    private int ID = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -48,11 +46,14 @@ public class Physics : MonoBehaviour
         //initialise some varaiables on startup;
         MaxPinDistance = 1;
         MaxFloorDistance = (ballRadius + Alley.transform.localScale.y/2);
-        
+        Ball.GetComponent<Pin_Properties>().Mass = 1;
+        foreach(GameObject pin in Pins)
+        {
+            pin.GetComponent<Pin_Properties>().LocalID = ID;
+            ID++;
+        }
 
-        
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -61,33 +62,44 @@ public class Physics : MonoBehaviour
             currentTime = Time.realtimeSinceStartup;
             dt = currentTime - OldTime;
             OldTime = currentTime;
-            //apply forces
-            //update positions and velocities
-            calcBallInitForce();
-            calcNewAcceleration();
-            calcVelocity();
+            if (Input.GetMouseButton(0))
+            {
+                Ball.GetComponent<Pin_Properties>().Velocity.x += -1.0f;
+               
+            }
+                //apply forces
+                //update positions and velocities
+            foreach (GameObject pin in Pins)
+            {
+
+                calcVelocity(pin);
+                calcAcceleration(pin);
+                Move(pin);
+                //if velocity is close to 0 stop moving(fixes floating point errors) TODO: Change to max static friction
+                if(((pin.GetComponent<Pin_Properties>().Velocity.x)*(pin.GetComponent<Pin_Properties>().Velocity.x) + (pin.GetComponent<Pin_Properties>().Velocity.y) * (pin.GetComponent<Pin_Properties>().Velocity.y)) < 0.05f)
+                {
+                    pin.GetComponent<Pin_Properties>().Velocity = new Vector2(0.0f, 0.0f);
+                }
+            }
             checkPositions(); //check for collisions
                               //solve constraints 
                               //update on screen
-            if (!collidedWithFloor)
+
+            //TODO: gravity 
+            //if (!collidedWithFloor)
+            //{
+            //    Ball.transform.position += new Vector3(0.0f, VectorVelocity.y * dt, 0.0f);
+            //}
+            //Ball.transform.position += new Vector3((Ball.GetComponent<Pin_Properties>().Velocity.x * dt), 0.0f, 0.0f);
+            if (!collidedWithPins)
             {
-                Ball.transform.position += new Vector3(0.0f, VectorVelocity.y * dt, 0.0f);
-            }
-            Ball.transform.position += new Vector3(VectorVelocity.x * dt, 0.0f, 0.0f);
-            if (collidedWithFloor && !collidedWithPins)
-            {
-                VectorVelocity.y = 0.0f;
-                GameCamera.transform.position += new Vector3(VectorVelocity.x * dt, 0.0f, 0.0f);
+                GameCamera.transform.position += new Vector3(Ball.GetComponent<Pin_Properties>().Velocity.x * dt, 0.0f, 0.0f);
 
                 if (!Roll.isPlaying)
                 {
                     Roll.Play();
                 }
 
-            }
-            if (collidedWithPins)
-            {
-                // CalculateCollisionAngle(PinXdistance);
             }
 
             currentTime += dt;
@@ -98,42 +110,36 @@ public class Physics : MonoBehaviour
   
     void calcBallInitForce()
     {
-        BallForce = (new Vector3(InitialXForce, 0, 0));
+        Ball.GetComponent<Pin_Properties>().Force = (new Vector3(InitialXForce, 0, 0));
     }
-
-    void calcNewAcceleration()
+    void calcVelocity(GameObject ThisObject)
     {
-        Ballacceleration = new Vector3(BallForce.x / mass, BallForce.y / mass, BallForce.z / mass);
+        ThisObject.GetComponent<Pin_Properties>().Velocity.x += ThisObject.GetComponent<Pin_Properties>().acceleration.x * dt;
+        ThisObject.GetComponent<Pin_Properties>().Velocity.y += ThisObject.GetComponent<Pin_Properties>().acceleration.y * dt;
+        //ThisObject.GetComponent<Pin_Properties>().Velocity.z += ThisObject.GetComponent<Pin_Properties>().acceleration.z * dt;
     }
-
-    void calcVelocity()
+    void calcAcceleration(GameObject ThisObject)
     {
-        VectorVelocity.x += Ballacceleration.x * dt;
-        VectorVelocity.y += Ballacceleration.y * dt;
-        VectorVelocity.z += Ballacceleration.z * dt;
+        ThisObject.GetComponent<Pin_Properties>().acceleration.x = -(ThisObject.GetComponent<Pin_Properties>().Velocity.x * 0.8f);
+        ThisObject.GetComponent<Pin_Properties>().acceleration.y = -(ThisObject.GetComponent<Pin_Properties>().Velocity.y * 0.8f);
+        //ThisObject.GetComponent<Pin_Properties>().acceleration.z = ThisObject.GetComponent<Pin_Properties>().Force.z / mass;
     }
-
-    void CalculateCollisionAngle(float distanceToPin)
+    void calcAccelerationDueToForce()
     {
-        float LengthB;
-        float LengthC;
-        float SinOfAngleB;
-        float SinOfAngleC;
-        LengthB = CollidedPin.transform.position.x - Ball.transform.position.x;
-        LengthC = Ball.transform.position.z - CollidedPin.transform.position.z;
-        SinOfAngleB = ((LengthB * 1)/ distanceToPin);
-        SinOfAngleC = ((LengthC * 1) / distanceToPin);
-        AngleB = Mathf.Asin(SinOfAngleB) * Mathf.Rad2Deg;
-        AngleC = Mathf.Asin(SinOfAngleC) * Mathf.Rad2Deg;
- 
+        Ball.GetComponent<Pin_Properties>().acceleration.x = (Ball.GetComponent<Pin_Properties>().Force.x / mass);
+        Ball.GetComponent<Pin_Properties>().acceleration.y = (Ball.GetComponent<Pin_Properties>().Force.y / mass);
+    }
+    void Move(GameObject ThisObject)
+    {
+        ThisObject.transform.position += new Vector3((ThisObject.GetComponent<Pin_Properties>().Velocity.x * dt), 0.0f, 0.0f);
+        ThisObject.transform.position += new Vector3(0.0f, 0.0f, (ThisObject.GetComponent<Pin_Properties>().Velocity.y * dt));
+        //ThisObject.transform.position += new Vector3(0.0f, 0.0f, (ThisObject.GetComponent<Pin_Properties>().Velocity.z * dt));
     }
 
     public void checkPositions()
     {
         //FloorDistance = Mathf.Abs(Alley.transform.position.y - Ball.transform.position.y);
             collidedWithFloor = true;
-            print(FloorDistance);
-            print("collidedWithFloor");
 
         if (Ball.transform.position.x < (Alley.transform.position.x -(Alley.transform.localScale.x/2)))
         { 
@@ -142,30 +148,67 @@ public class Physics : MonoBehaviour
 
         foreach (GameObject pin in Pins)
         {
-            Pindistance = Mathf.Abs(Vector3.Distance(Ball.transform.position, pin.transform.position));
-            if (Pindistance <= MaxPinDistance)
-            { 
-                //fix overlaps
-                float DistanceA = Mathf.Sqrt((Ball.transform.position.x - pin.transform.position.x)*(Ball.transform.position.x - pin.transform.position.x)
-                    + (Ball.transform.position.z - pin.transform.position.z) * (Ball.transform.position.z - pin.transform.position.z));
-                collidedWithPins = true;
-                float DistanceOV = 0.5f * (DistanceA - 1.0f);
-                //tranform by half the overlap in the vector direction of the collision
-                Ball.transform.position -= new Vector3(DistanceOV * (Ball.transform.position.x - pin.transform.position.x) / DistanceA, 0, DistanceOV * (Ball.transform.position.z - pin.transform.position.z) / DistanceA);
-                pin.transform.position += new Vector3(DistanceOV * (Ball.transform.position.x - pin.transform.position.x) / DistanceA, 0, DistanceOV * (Ball.transform.position.z - pin.transform.position.z) / DistanceA);
-                
-                collisionTime = currentTime;
-                CollidedPin = pin;
-                print("collidedWithpin");
-                Roll.Stop();
-                if (!Strike.isPlaying)
+            foreach(GameObject Other in Pins)
+            {
+                if (pin.GetComponent<Pin_Properties>().LocalID != Other.GetComponent<Pin_Properties>().LocalID)
                 {
-                   Strike.Play();
-                }
+                    Pindistance = Mathf.Abs(Vector3.Distance(pin.transform.position, Other.transform.position));
+                    if (Pindistance <= MaxPinDistance)
+                    {
+                        //fix overlaps
+                        
+                        float DistanceA = Mathf.Sqrt((pin.transform.position.x - Other.transform.position.x) * (pin.transform.position.x - Other.transform.position.x)
+                            + (pin.transform.position.z - Other.transform.position.z) * (pin.transform.position.z - Other.transform.position.z));
+                        CalcDynamic(pin, Other, DistanceA);
+                        collidedWithPins = true;
+                        float DistanceOV = 0.5f * (DistanceA - 1.0f);
+                        //tranform by half the overlap in the vector direction of the collision
+                        pin.transform.position -= new Vector3(DistanceOV * (pin.transform.position.x - Other.transform.position.x) / DistanceA, 0, DistanceOV * (pin.transform.position.z - Other.transform.position.z) / DistanceA);
+                        Other.transform.position += new Vector3(DistanceOV * (pin.transform.position.x - Other.transform.position.x) / DistanceA, 0, DistanceOV * (pin.transform.position.z - Other.transform.position.z) / DistanceA);
+                        CollidedPin = pin;
+                        print("collidedWithpin");
+                        Roll.Stop();
+                        if (!Strike.isPlaying && CollidedPin.GetComponent<Pin_Properties>().LocalID == 10)
+                        {
+                            Strike.Play();
+                        }
+                       
+                        
+                    }
+               
 
+                }
             }
+           
 
         }
             
     }
+    void CalcDynamic(GameObject p1, GameObject p2, float DistanceB)
+    {
+        //Normal Reaction
+        float Normalx = (p2.transform.position.x - p1.transform.position.x) / DistanceB;
+        float Normaly = (p2.transform.position.z - p1.transform.position.z) / DistanceB;
+
+        //Tangent Reaction
+        float Tangentx = -Normaly;
+        float Tangenty = Normalx;
+
+        //Tangental Dot Product
+        float TDP1 = p1.GetComponent<Pin_Properties>().Velocity.x * Tangentx + p1.GetComponent<Pin_Properties>().Velocity.y * Tangenty;
+        float TDP2 = p2.GetComponent<Pin_Properties>().Velocity.x * Tangentx + p2.GetComponent<Pin_Properties>().Velocity.y * Tangenty;
+
+        float NDP1 = p1.GetComponent<Pin_Properties>().Velocity.x * Normalx + p1.GetComponent<Pin_Properties>().Velocity.y * Normaly;
+        float NDP2 = p2.GetComponent<Pin_Properties>().Velocity.x * Normalx + p2.GetComponent<Pin_Properties>().Velocity.y * Normalx;
+
+        //Conservation of Momentum for elastic collision
+        float Momentum1 = (NDP1 * (p1.GetComponent<Pin_Properties>().Mass - p2.GetComponent<Pin_Properties>().Mass) + 2.0f * p2.GetComponent<Pin_Properties>().Mass * NDP2) / p1.GetComponent<Pin_Properties>().Mass * p2.GetComponent<Pin_Properties>().Mass;
+        float Momentum2 = (NDP2 * (p2.GetComponent<Pin_Properties>().Mass - p1.GetComponent<Pin_Properties>().Mass) + 2.0f * p1.GetComponent<Pin_Properties>().Mass * NDP1) / p1.GetComponent<Pin_Properties>().Mass * p2.GetComponent<Pin_Properties>().Mass;
+        //Update Velocities
+        p1.GetComponent<Pin_Properties>().Velocity.x = Tangentx * TDP1 + Normalx * Momentum1;
+        p1.GetComponent<Pin_Properties>().Velocity.y = Tangenty * TDP1 + Normaly * Momentum1;
+        p2.GetComponent<Pin_Properties>().Velocity.x = Tangentx * TDP2 + Normalx * Momentum2;
+        p2.GetComponent<Pin_Properties>().Velocity.y = Tangenty * TDP2 + Normaly * Momentum2;
+    }
+
 }
--- Assets/Pin_Properties.cs
+++ Assets/Pin_Properties.cs
@@ -5,16 +5,10 @@ using UnityEngine;
 public class Pin_Properties : MonoBehaviour
 {
 
-    public Vector3 Velocity;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Vector2 Velocity = new Vector3(0.0f,0.0f);
+    public Vector2 acceleration = new Vector3(0.0f, 0.0f);
+    public Vector2 Force = new Vector3(0.0f, 0.0f);
+    public int Mass = 1;
+    public int LocalID;
+   
 }
